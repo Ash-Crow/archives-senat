cebf040c72cdc9f366cc7d045bb7b7fd32e17a79
Sylvain Boissel <sylvain@ashtree.eu>
2016-04-01 12:14:16 +0200

    Recollection with Wikidata

--- senat-communaute/extract-identite.py
+++ senat-communaute/extract-identite.py
@@ -3,9 +3,23 @@
 
 import csv
 import re
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 path = "/home/sylvain/Dropbox/jeux de données/Archives du Sénat/"
 filename = "senateurs_communaute_wd-identite.csv"
+
+
+def wikidata_sparql_query(query):
+    """
+    Queries WDQS and returns the result
+    """
+    endpoint = "https://query.wikidata.org/bigdata/namespace/wdq/sparql"
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setQuery(query)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results
 
 
 def date_qs(date):
@@ -38,15 +52,47 @@ def cleaner(text):
     text = text.replace(' La ', ' la ')
     text = text.replace(' Et ', ' et ')
     text = text.replace('-Et-', '-et-')
+    text = text.replace('Correze', 'Corrèze')
+    text = text.replace('Correze', 'Corrèze')
     text = re.compile('France (.*)').sub('France', text)
     return text
 
+
+def get_qids():
+    results = wikidata_sparql_query(
+        """
+        SELECT DISTINCT ?senateur ?senateurLabel ?idSenat ?dateNaissance
+        ?debutMandat ?finMandat ?delegueparLabel ?groupeLabel {
+        ?senateur wdt:P31 wd:Q5 .
+        ?senateur wdt:P39 wd:Q20177062 .
+        ?senateur wdt:P1808 ?idSenat .
+        FILTER (SUBSTR(str(?idSenat), 1, 19) = "senateur-communaute")
+
+        SERVICE wikibase:label {
+        bd:serviceParam wikibase:language "fr" .
+        }
+        } ORDER BY ?idSenat
+        """)
+
+    senateurs = {}
+    for r in results['results']['bindings']:
+        name = r['senateurLabel']['value']
+        idSenat = r['idSenat']['value']
+        entity = r['senateur']['value']
+        qid = (entity).split('/')[-1]
+        senateurs[name] = {'qid': qid, 'idSenat': idSenat}
+
+    return senateurs
+
+senateurs = get_qids()
 with open(path + filename, 'r') as csv_input:
     with open(path + 'out.csv', 'w') as csv_output:
         reader = csv.DictReader(csv_input)
         fieldnames = reader.fieldnames[:]
         fieldnames.remove("Année de naissance")
         fieldnames.remove("NOM TRI")
+        fieldnames.append('qid')
+        fieldnames.append('idSenat')
         writer = csv.DictWriter(csv_output,
                                 fieldnames=fieldnames,
                                 extrasaction='ignore')
@@ -63,4 +109,10 @@ with open(path + filename, 'r') as csv_input:
                 row["Date de naissance"] = date_qs(row["Année de naissance"])
             else:
                 row["Date de naissance"] = date_qs(row["Date de naissance"])
+
+            name = "{} {}".format(row["PRENOM"], row["NOM"])
+            if name in senateurs.keys():
+                row['qid'] = senateurs[name]['qid']
+                row['idSenat'] = senateurs[name]['idSenat']
+
             writer.writerow(row)
